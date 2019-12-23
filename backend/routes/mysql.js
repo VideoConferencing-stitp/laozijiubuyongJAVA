3e39a3cddc631a4b00b406e7f99e19b768b41112
Xiang <1073739700@qq.com>
2019-12-24 00:32:00 +0800

    [+]:用Promise封装mysql数据

--- backend/routes/mysql.js
+++ backend/routes/mysql.js
@@ -2,29 +2,29 @@ const router = require('koa-router')()
 const mysql = require('mysql');
 
 router.get('/search', async (ctx, next) => {
-    // var connection = mysql.createConnection({
-    //     host     : 'localhost',
-    //     user     : 'root',
-    //     password : '263785czx',
-    //     database : 'salesdb'
-    // });
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '263785czx',
+        database : 'salesdb'
+    });
     
-    // connection.connect();
+    connection.connect();
     
-    // var  sql = 'SELECT * FROM sale_backup';
+    var sql = 'SELECT * FROM sale_backup';
     
-    // connection.query(sql,function (err, result) {
-    //     if(err){
-    //       console.log('[SELECT ERROR] - ',err.message);
-    //       return;
-    //     }
- 
-    //    console.log('--------------------------SELECT----------------------------');
-    //    console.log(result);
-    //    console.log('------------------------------------------------------------\n\n');  
-    // });
+    let query = ()=>{
+      return new Promise((resolve,reject)=>{
+          connection.query(sql,(err,data) => {
+              if(err){
+                  resolve({message:err.message})
+              }
+              resolve(data);
+          })
+      })
+    }
+    let result = await query();
+    ctx.body = result;
+})
 
-    ctx.body = 'result'
-  })
-
-  module.exports = router
+module.exports = router
